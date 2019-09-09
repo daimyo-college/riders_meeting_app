9f6e2a4dca13c8af20fe1959a2cf42273683a921
hirokato <hirok803@gmail.com>
2019-09-09 18:01:15 +0900

    reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_050006) do
+ActiveRecord::Schema.define(version: 2019_09_09_014400) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_050006) do
 
   create_table "riders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "rider_name"
+    t.string "email"
     t.string "bike_name"
     t.string "icon_file_name"
     t.datetime "created_at", null: false
