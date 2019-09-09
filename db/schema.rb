e86640376274a09610070c9efe25b58f7145b04e
hirokato <hirok803@gmail.com>
2019-09-09 14:54:27 +0900

    ログインのリンクを訂正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_014400) do
+ActiveRecord::Schema.define(version: 2019_09_09_051422) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
@@ -24,11 +24,17 @@ ActiveRecord::Schema.define(version: 2019_09_09_014400) do
 
   create_table "riders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "rider_name"
-    t.string "email"
     t.string "bike_name"
     t.string "icon_file_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_riders_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_riders_on_reset_password_token", unique: true
   end
 
   create_table "touring_routes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
